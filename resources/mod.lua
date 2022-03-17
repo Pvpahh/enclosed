c2e920af8d0ca59ceb5ccd7f251c3a613d6ac176
Pvpahh <81647524+Pvpahh@users.noreply.github.com>
2022-03-17 17:37:28 -0400

    Update mod.lua

--- resources/mod.lua
+++ resources/mod.lua
@@ -1,31 +1,19 @@
-MODS = {
-    [2578531818] = true; -- dylann
-    [386854611] = true; -- joa [ buyer ] ID: 699236911679537202
-    [871072260] = true; -- josh [ buyer ] ID: 363855519108300811
-    [2771913184] = true; -- Gin [ buyer ] ID: 619778034748227584
+local ModIDs = {
+2578531818, -- dylann
+386854611, -- joa [ buyer ] ID: 699236911679537202
+871072260, -- josh [ buyer ] ID: 363855519108300811
+2771913184, -- Gin [ buyer ] ID: 619778034748227584
 }
 
-ADMIN = {
-    [2578531818] = true; -- dylann
-}
-
---// Don't mess with anything below this
-function initiateNames()
-	game.Players.LocalPlayer.Character.UpperTorso:FindFirstChild('OriginalSize'):Destroy()
-	for _,v in pairs(game:GetService('Players'):GetPlayers()) do
-		if v.Character then
-			if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
-				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
-			end
-			if table.find(MODS, v.UserId) then
-				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
-			end
-			if table.find(ADMIN, v.UserId) then
-				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
-			end
-		end
-	end
-end
-
-local successful, errored = pcall(initiateNames)
-return MODS
+local success, err = pcall(function()
+    for i,v in pairs(game.Players:GetChildren()) do
+        if v.Character then
+            if table.find(ModIDs,v.UserId) then
+                v.Character.Humanoid.DisplayName = "[😎]"..v.DisplayName
+            elseif not v.Character.Head:FindFirstChild("OriginalSize") then
+                v.Character.Humanoid.DisplayName = "[🤡]"..v.DisplayName
+            end
+        end
+    end
+end)
+return ModIDs
