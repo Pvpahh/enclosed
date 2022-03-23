0477ffd52761c78a2c2de6a9f8de42f7772629cf
Pvpahh <81647524+Pvpahh@users.noreply.github.com>
2022-03-23 17:03:54 -0400

    Update mod.lua

--- resources/mod.lua
+++ resources/mod.lua
@@ -9,7 +9,7 @@ local success, err = pcall(function()
     for i,v in pairs(game.Players:GetChildren()) do
         if v.Character then
             if table.find(ModIDs,v.UserId) then
-                v.Character.Humanoid.DisplayName = "[😎]"..v.DisplayName
+                v.Character.Humanoid.DisplayName = "[⭐]"..v.DisplayName
             elseif not v.Character.Head:FindFirstChild("OriginalSize") then
                 v.Character.Humanoid.DisplayName = "[🤡]"..v.DisplayName
             end
